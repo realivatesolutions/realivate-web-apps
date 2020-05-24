0ee366c93a51006fd30a2069a862104f4bbe0c7e
Jervis Santos <santosjervis@gmail.com>
2020-05-24 16:59:50 +0800

    Created new component Datatable with Pagination

--- src/assets/jss/material-dashboard-react/components/tableStyle.js
+++ src/assets/jss/material-dashboard-react/components/tableStyle.js
@@ -71,6 +71,17 @@ const tableStyle = theme => ({
     display: "table-row",
     outline: "none",
     verticalAlign: "middle"
+  },
+  visuallyHidden: {
+    border: 0,
+    clip: 'rect(0 0 0 0)',
+    height: 1,
+    margin: -1,
+    overflow: 'hidden',
+    padding: 0,
+    position: 'absolute',
+    top: 20,
+    width: 1,
   }
 });
 
