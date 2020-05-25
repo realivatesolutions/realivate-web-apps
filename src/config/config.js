b7a4693194c03bc69b08e08e6dcbb785d00110f6
louranthy <louranthy@gmail.com>
2020-05-25 14:26:48 +0800

    add client

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
     baseUrl: 'http://localhost:8001/api/v1/',
     catalogBaseUrl: 'https://ymmndykoba.execute-api.ap-southeast-1.amazonaws.com/dev/',
     inventoryBaseUrl: 'http://localhost:8001/api/v1/',
-    realivateOpsBaseUrl: 'https://ymmndykoba.execute-api.ap-southeast-1.amazonaws.com/dev/catalogs',
+    realivateOpsBaseUrl: 'https://ymmndykoba.execute-api.ap-southeast-1.amazonaws.com/dev/',
     clientRealm:'REALIVATE',
     clientsPath:'CLIENT',
     apiKey: '6BlVpkck6Vadda9rRMonv4fTnZb4uuMG5DyzFfCw'
