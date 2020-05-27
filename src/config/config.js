a9e43e174eb5afadbcb72dd884121fd898be3b10
louranthy <louranthy@gmail.com>
2020-05-28 07:51:47 +0800

    added apikey to axios header

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     baseUrl: 'http://localhost:8001/api/v1/',
-    catalogBaseUrl: 'https://cq58zupax4.execute-api.ap-southeast-1.amazonaws.com/dev/',
-    inventoryBaseUrl: 'http://localhost:8001/api/v1/'
+    catalogBaseUrl: 'https://ymmndykoba.execute-api.ap-southeast-1.amazonaws.com/dev/',
+    inventoryBaseUrl: 'http://localhost:8001/api/v1/',
+    apiKey: '6BlVpkck6Vadda9rRMonv4fTnZb4uuMG5DyzFfCw'
 }
--- src/utils/request/index.js
+++ src/utils/request/index.js
@@ -1,15 +1,17 @@
 import axios from 'axios'
-
+import config  from '../../config/config';
 const getInstance = (baseUrl) => {
+    let apiKey = config.apiKey;
     let instance = axios.create({
         baseURL: baseUrl,
         timeout: 100000,
         xsrfHeaderName: 'x-csrf-token'
     })
-
+    
     instance.interceptors.request.use((config) => {
         config.headers.post['Content-Type'] = 'application/json';
         config.headers.put['Content-Type'] = 'application/json';
+        config.headers.common['x-api-key'] = apiKey;
         return config
     }, (error) => {
         return Promise.reject(error)
